c7fd07062819bf2361ff38e13c8b6acd8911f303
Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>
2026-05-21 10:38:42 +0530

    Add files via upload
    Signed-off-by: Riya jatav <233461719+riyajatav86-alt@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+//pure function 
+function double(){
+    return num*2
+
+}
+//console.log(double(5))//10
+//console.log(double(5))//10
+let count=0
+function fun1(){
+    count++
+    return count
+}
---
+++ new.js
@@ -0,0 +1,8 @@
+//let a=5
+// export let b=10
+//export default
+let data="hello"
+function sum(a,b){
+    console.log(a+b)
+}
+export (data,sum)
