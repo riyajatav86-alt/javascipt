ff572463bf2c63fb8a617c190717f1ca361f6f01
Riya <12345678+username@users.noreply.github.com>
2026-05-21 10:37:10 +0530

    index js

--- .vscode/lec12/index.js
+++ .vscode/lec12/index.js
@@ -1,2 +1,12 @@
-import data from "./new.js"
-console.log(data)
+//pure function 
+function double(){
+    return num*2
+
+}
+//console.log(double(5))//10
+//console.log(double(5))//10
+let count=0
+function fun1(){
+    count++
+    return count
+}
